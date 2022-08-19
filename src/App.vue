97490bd887e1841ed5368c1f4691ca91c73b318e
Dmitry Kvasov <dmitry@Dmitrys-MacBook-Pro.local>
2022-08-19 20:44:47 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="addLike">Like</button>
+    <button @click="addDislike">Dislike</button>
+    <div>Count likes {{ likes }}</div>
+    <div>Count dislikes {{ dislikes }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
